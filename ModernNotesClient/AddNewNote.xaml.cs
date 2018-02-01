6cfeefe972a1f6364d3cee0b2070a54b122a4af3
franzliszt <stianpiano@gmail.com>
2018-02-01 20:39:44 +0100

    Update text works.

--- ModernNotesClient/AddNewNote.xaml.cs
+++ ModernNotesClient/AddNewNote.xaml.cs
@@ -1,21 +1,7 @@
 ﻿using ModernNotes.Client.Models;
 using ModernNotesClient.Controller;
-using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ModernNotesClient
 {
@@ -24,22 +10,20 @@ namespace ModernNotesClient
     /// </summary>
     public partial class AddNewNote : Page
     {
-        private readonly string _baseUri = "http://localhost:51382/";
-
         public AddNewNote()
         {
             InitializeComponent();
-            textBox.Clear();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {//
+        {
             var note = new NoteRequest(textBox.Text);
-
             var isSaved = new ModernNotesClientController().PostNewNote(note);
             if (isSaved)
+            {
+                textBox.Clear();
                 this.NavigationService.Navigate(new ViewMyNotes());
-            
+            }
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
--- ModernNotesClient/ViewMyNotes.xaml.cs
+++ ModernNotesClient/ViewMyNotes.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using ModernNotes.Client.Model;
-using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,7 +14,6 @@ namespace ModernNotesClient
     {
         private readonly ModernNotesClientController _controller;
         private List<Note> _notes;
-        private TextBox _text;
 
         public ViewMyNotes()
         {
@@ -33,67 +31,77 @@ namespace ModernNotesClient
                 _notes = new List<Note>();
 
             var grid = new Grid();
-
-            var gridCol0 = new ColumnDefinition();
-            var gridCol1 = new ColumnDefinition();
-            var gridCol2 = new ColumnDefinition();
-
-            grid.ColumnDefinitions.Add(gridCol0);
-            grid.ColumnDefinitions.Add(gridCol1);
-            grid.ColumnDefinitions.Add(gridCol2);
-
             var rowNumber = 0;
-            _notes.ForEach(note =>
+
+           _notes.ForEach(note =>
             {
                 var row = new RowDefinition();
                 grid.RowDefinitions.Add(row);
 
-                _text = new TextBox { Text = note.Timestamp + "\n" + note.NoteText, Name = "textBox" };
-                var deleteBtn = new Button
-                {
-                    Content = "Delete Note",
-                    CommandParameter = note.Id,
-                };
-                deleteBtn.Click += DeleteBtn_Click;
-                var updateBtn = new Button
-                {
-                    Content = "Update Note",
-                    CommandParameter = note.Id
-                };
-                updateBtn.Click += UpdateBtn_Click;
+                var stackpanel = new StackPanel { Orientation = Orientation.Horizontal };
+                var text = new TextBox { Text = note.NoteText };
 
-                Grid.SetRow(_text, rowNumber);
-                Grid.SetColumn(_text, 0);
-                Grid.SetRow(deleteBtn, rowNumber);
-                Grid.SetColumn(deleteBtn, 1);
-                Grid.SetRow(updateBtn, rowNumber);
-                Grid.SetColumn(updateBtn, 2);
+                Button updateButton = CreateUpdateButton(note);
+                Button deleteButton = CreateDeleteButton(note);
+                
+                stackpanel.Children.Add(text);
+                stackpanel.Children.Add(updateButton);
+                stackpanel.Children.Add(deleteButton);
 
-                grid.Children.Add(_text);
-                grid.Children.Add(deleteBtn);
-                grid.Children.Add(updateBtn);
+                Grid.SetRow(stackpanel, rowNumber);
+                grid.Children.Add(stackpanel);
 
                 rowNumber++; // Next row.
             });
             scroll.Content = grid;
         }
 
+        private Button CreateDeleteButton(Note note)
+        {
+            var deleteBtn = new Button
+            {
+                Content = "Delete Note",
+                Tag = note.Id,
+            };
+            deleteBtn.Click += DeleteBtn_Click;
+
+            return deleteBtn;
+        }
+
+        private Button CreateUpdateButton(Note note)
+        {
+            var updateBtn = new Button
+            {
+                Content = "Update Note",
+                Tag = note.Id
+            };
+            updateBtn.Click += UpdateBtn_Click;
+
+            return updateBtn;
+        }
+
         private void DeleteBtn_Click(object sender, RoutedEventArgs e)
         {
-            var id = ((Button)sender).CommandParameter;
-            
-            _notes = _controller.DeleteNote((int)id);
+            Button button = sender as Button;
+            _notes = _controller.DeleteNote((int) button.Tag);
             
             SetupView();
         }
-        
+
         private void UpdateBtn_Click(object sender, RoutedEventArgs e)
         {
-            var id = ((Button)sender).CommandParameter;
+            var button = sender as Button;
+            var parent = button.Parent as StackPanel;
 
-            var updateText = _text.Text;
+            if (parent == null)
+                return;
+
+            TextBox textBoxChild = parent.Children[0] as TextBox;
+
+            int id = (int)button.Tag;
+            var updateText = textBoxChild.Text;
+
             var request = new NoteRequest(updateText);
-
             _notes = _controller.UpdateNote(request,(int) id);
 
             GoToAddNoteView();
